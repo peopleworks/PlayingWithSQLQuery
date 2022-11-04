850d4f13b907f61b7846a08ae501a933ff386814
Pedro Hernández <peopleworks@gmail.com>
2022-11-04 12:31:06 -0400

    Create databaseobjects.sql

---
+++ databaseobjects.sql
@@ -0,0 +1,19 @@
+-- Database Objects MS SQL Server
+
+
+-- Information about table -- 
+SELECT * FROM sys.sysobjects WHERE xtype = 'U'
+
+-- Information about Stored Procedure --
+SELECT * FROM sys.sysobjects WHERE xtype = 'P'
+
+-- Information about Functions --
+SELECT * FROM sys.sysobjects WHERE xtype = 'FN'
+
+-- Information about Views --
+SELECT * FROM sys.sysobjects WHERE xtype = 'V'
+
+
+
+
+
