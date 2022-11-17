7988f3993c894ca05d41f00b9ecc0368a45c48d5
peopleworks <peopleworks@gmail.com>
2022-11-17 16:04:47 -0500

    Free SQL Server Memory
    Free SQL Server Memory

---
+++ MS SQL Server/FreeSQLServerMemory.sql
@@ -0,0 +1,29 @@
+USE [master]
+GO
+/****** Object:  StoredProcedure [dbo].[CommandCleanMemory]    
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE [dbo].[CommandCleanMemory]
+AS
+
+BEGIN
+   
+   
+  
+	Exec sp_configure 'show advanced options', 1 
+	RECONFIGURE 
+	 
+	/*** Drop the max down to 64GB temporarily ***/
+	Exec sp_configure 'max server memory', 10240 -- 10 GB
+	RECONFIGURE 
+	/**** Wait a couple minutes to let SQLServer to naturally release the RAM..... ****/
+	WAITFOR DELAY '00:02:00' 
+
+	/** now bump it back up to "lots of RAM"! ****/
+	Exec sp_configure 'max server memory', 32768 -- 32 GB
+	RECONFIGURE
+
+END
