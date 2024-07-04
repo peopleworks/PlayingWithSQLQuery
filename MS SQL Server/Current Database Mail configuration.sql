85cc8f7c0c71bbe614b05271322e099b0678f702
peopleworks <peopleworks@gmail.com>
2024-07-04 18:18:03 -0400

    SQL Server Mail Info
    SQL Server Mail Info

---
+++ MS SQL Server/Current Database Mail configuration.sql
@@ -0,0 +1,7 @@
+-- Current Database Mail configuration?
+
+EXEC msdb.dbo.sysmail_help_configure_sp;
+EXEC msdb.dbo.sysmail_help_account_sp;
+EXEC msdb.dbo.sysmail_help_profile_sp;
+EXEC msdb.dbo.sysmail_help_profileaccount_sp;
+EXEC msdb.dbo.sysmail_help_principalprofile_sp;
